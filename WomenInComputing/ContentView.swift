4c7ea9642d0c2f23f40490d14b692d3f765af8a1
Nick Nedjat <nick.nedjat@icloud.com>
2026-02-15 11:42:34 -0500

    Initial Commit

---
+++ WomenInComputing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WomenInComputing
+//
+//  Created by Nick Nedjat on 15.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by Nick Nedjat on 15.02.26.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
